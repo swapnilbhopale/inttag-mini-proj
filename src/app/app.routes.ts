255b240b51c55c696ffb0c7ea6ac0a069153bd42
swapnilbhopale <bhopaleswapnil3@gmail.com>
2026-03-17 14:39:04 +0530

    bootstrap added, preAuth, postAuth, login, register cmponent created.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './preAuth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login', component:Login},
+    {path:'', redirectTo:'login', pathMatch:'full'},
+
+];
