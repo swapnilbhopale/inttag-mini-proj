551660953ffabfbca466ae3d701ef826387cee87
swapnilbhopale <bhopaleswapnil3@gmail.com>
2026-03-17 16:08:08 +0530

    changes done.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './preAuth/login/login';
+import { Register } from './preAuth/register/register';
 
 export const routes: Routes = [
     {path:'login', component:Login},
     {path:'', redirectTo:'login', pathMatch:'full'},
+    {path:'register', component:Register},
 
 ];
