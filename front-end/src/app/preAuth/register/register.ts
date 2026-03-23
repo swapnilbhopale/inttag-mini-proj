09c8d5e936ccb71c1f5a2e5f02d092e46b975329
swapnilbhopale <bhopaleswapnil3@gmail.com>
2026-03-23 14:10:58 +0530

    registration fuctionality done api working.

--- front-end/src/app/preAuth/register/register.ts
+++ front-end/src/app/preAuth/register/register.ts
@@ -33,8 +33,8 @@ export class Register {
         this.registerForm.reset();
       },
 
-      error: (err) => {
-        alert(`An error occurred: ${err}`);
+      error: (res: any) => {
+        alert(`An error occurred: ${res.error.message}`);
       },
     });
   }
