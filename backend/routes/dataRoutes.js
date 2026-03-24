3bab91773f4e096414075ef17d0a598c93bb8cf2
swapnilbhopale <bhopaleswapnil3@gmail.com>
2026-03-24 10:29:25 +0530

    dataRoute created

--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -7,4 +7,44 @@ const router = express.Router();
 router.post("/create", async (req, res) => {
   const { name, email, designation, phone, address } = req.body;
   const session = driver.session();
+  try {
+    await session.run(
+      `CREATE (e:Employee {
+      name: $name,
+      email: $email,
+      designation: $designation,
+      phone: $phone,
+      address: $address
+      })
+      `, { name, email, designation, phone, address }
+    )
+    res.json({ message: "Employee created successfully" })
+  }
+  catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+  finally {
+    await session.close()
+  }
 });
+
+
+// get data
+router.get('/data', async (req, res) => {
+  const session = driver.session();
+
+  try {
+    const result = await session.run(
+      "MATCH (e:Employee) RETURN e"
+    );
+
+    const employess = result.records.map(records => records.get('e').properties)
+    res.json(employess)
+  }
+  catch (err) {
+    res.status(500).json({ message: err.message })
+  } finally {
+    session.close()
+  }
+})
+module.exports = router
